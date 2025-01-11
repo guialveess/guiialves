1688d06a39da144faf5bfc4d887ca40e3827cf0d
Guilherme Alves <97guilherme.alves@gmail.com>
2025-01-11 14:44:04 -0300

    uselenis modified in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,18 @@
 
 import { bricolageGrotesque } from "@/app/font";
 import { useLenis } from "@/hooks/useLenis";
-import { MDXProvider } from "@mdx-js/react";
 import { ViewTransitions } from "next-view-transitions";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
-  useLenis(): void;
 }) {
+  // Ativa o Lenis para scroll suave
+  useLenis();
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={bricolageGrotesque.className}>
